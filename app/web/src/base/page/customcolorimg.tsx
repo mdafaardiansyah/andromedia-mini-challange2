fc4fe67a7573424b01c521d830c42139485b887b
Muhammad Dafa Ardiansyah <71364076+mdafaardiansyah@users.noreply.github.com>
2022-09-30 15:06:54 +0700

    Can Select Seed and Upload Img, minus Color Picker

--- app/web/src/base/page/customcolorimg.tsx
+++ app/web/src/base/page/customcolorimg.tsx
@@ -1,7 +1,7 @@
-import { page } from 'web-init'
+import {page} from 'web-init'
 import {useEffect, useRef, useState} from "react";
 import Navbar from "src/components/Navbar";
-import WindahFace from "src/assets/images/WindahFace.png";
+import WallInteriorImg from "src/assets/images/walldesign_1.jpg";
 import Fotoku from "src/assets/images/Fotoku_2.png";
 import OpenCVLogo from "src/assets/logo/opencv-logo-small.png";
 //@ts-ignore
@@ -10,102 +10,144 @@ import "mirada";
 
 
 export default page({
-  url: '/customcolorimg',
-  component: ({}) => {
-    const img = useRef(null);
-    const [openCVLoaded, setOpenCVLoaded] = useState(false);
-    const [imgSrcLoaded, setImgSrcLoaded] = useState(false);
+    url: '/customcolorimg',
+    component: ({}) => {
+        const img = useRef<HTMLCanvasElement>(null);
+        const imgOutput = useRef<HTMLCanvasElement>(null);
+        const [openCVLoaded, setOpenCVLoaded] = useState(false);
+        const [imgSrcLoaded, setImgSrcLoaded] = useState(false);
+        const [fileUploaded, setFileUploaded] = useState<File>();
+        const [color, setImgSrc] = useState<string>();
 
-    useEffect(()=> {
-        if (!openCVLoaded){
-            const script = InjectScript("opencv","https://docs.opencv.org/4.5.2/opencv.js");
-            script.then(()=>{
-                cv['onRuntimeInitialized'] = () => {
-                    setOpenCVLoaded(true);
+        useEffect(() => {
+            if (fileUploaded != undefined) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    if (e.target?.result) {
+                        const image = new Image();
+                        image.src = e.target.result.toString();
+                        image.onload = () => {
+                            const canvas = img.current!
+                            canvas.width = image.width;
+                            canvas.height = image.height;
+                            const context2D = canvas.getContext('2d')!;
+                            context2D.drawImage(image, 0, 0);
+
+                            const canvas2 = imgOutput.current!
+                            canvas2.width = image.width;
+                            canvas2.height = image.height;
+                            const context2D2 = canvas2.getContext('2d')!;
+                            context2D2.drawImage(image, 0, 0);
+                            setImgSrcLoaded(true);
+                        }
+                    }
                 }
-            }).catch(()=>{
-                console.log("error");
-            })
-        }
-    }, [])
+                reader.readAsDataURL(fileUploaded);
+            }
+        }, [fileUploaded]);
 
-    useEffect(()=>{
-        if (openCVLoaded && imgSrcLoaded){
-            const src = cv.imread(img.current!);
-            const dst = new cv.Mat();
-            cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
-            cv.imshow('canvasOutput1', dst);
-            src.delete();
-            dst.delete();
-        }
-    }, [openCVLoaded, imgSrcLoaded])
+        imgOutput.current?.addEventListener('mousedown', (e) => {
+            const rect = imgOutput.current?.getBoundingClientRect();
+            const scaleX = imgOutput.current!.width! / rect!.width!;
+            const scaleY = imgOutput.current!.height! / rect!.height!;
+            const x = (e.clientX - rect!.left!) * scaleX;
+            const y = (e.clientY - rect!.top!) * scaleY;
 
-    useEffect(()=>{
-        if (openCVLoaded &&imgSrcLoaded){
-            const src = cv.imread(img.current!);
-            const dst = new cv.Mat();
-            const low = new cv.Mat(src.rows, src.cols, src.type(), [0, 0, 0, 0]);
-            const high = new cv.Mat(src.rows, src.cols, src.type(), [150, 150, 150, 255]);
-// You can try more different parameters
-            cv.inRange(src, low, high, dst);
-            cv.imshow('canvasOutput2', dst);
-            src.delete(); dst.delete(); low.delete(); high.delete();
-        }
-    }, [openCVLoaded, imgSrcLoaded])
+            const imgProc = cv.imread(imgOutput.current!);
+            const rgbImg = new cv.Mat();
+            cv.cvtColor(imgProc, rgbImg, cv.COLOR_RGBA2RGB);
+            const grayImg = new cv.Mat();
+            cv.cvtColor(rgbImg, grayImg, cv.COLOR_RGB2GRAY);
+            const cannyGrey = new cv.Mat();
+            cv.Canny(grayImg, cannyGrey, 30, 75, 3);
+            const mask = cv.matFromArray(3, 3, cv.CV_8U, [0, -1, 0, -1, 5, 11, 0, -1, 0]);
 
-      useEffect(()=>{
-        if (openCVLoaded &&imgSrcLoaded){
-            let src = cv.imread(img.current!);
-            let dst = new cv.Mat();
-            cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
-// You can try more different parameters
-            cv.Canny(src, dst, 50, 100, 3, false);
-            cv.imshow('canvasOutput3', dst);
-            src.delete(); dst.delete();
-        }
-    }, [openCVLoaded, imgSrcLoaded])
+            const hsvImg = new cv.Mat();
+            cv.cvtColor(rgbImg, hsvImg, cv.COLOR_RGB2HSV);
+            const list = new cv.MatVector();
+            cv.split(hsvImg, list);
+            const schannel = new cv.Mat();
+            const slist = new cv.MatVector();
+            slist.push_back(list.get(1));
+            cv.merge(slist, schannel);
+            cv.medianBlur(schannel, schannel, 3);
+
+            const cannyS = new cv.Mat();
+            cv.Canny(schannel, cannyS, 30, 75, 3);
+            cv.addWeighted(cannyS, 0.5, cannyGrey, 0.5, 0, cannyS);
+            cv.dilate(cannyS, cannyS, mask, new cv.Point(-1, -1), 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
 
 
-    return (
-        <>
-          <Navbar/>
-          <div className="bg-blue w-full bg-cover px-6 lg:px-0 min-h-screen flex flex-col items-center ">
+            const coordinateSeedPoint = new cv.Point(x, y);
+            cv.resize(cannyS, cannyS, new cv.Size(cannyS.cols + 2, cannyS.rows + 2));
+            cv.floodFill(rgbImg, cannyS, coordinateSeedPoint, new cv.Scalar(255, 255, 255), new cv.Rect(), new cv.Scalar(20, 20, 20), new cv.Scalar(20, 20, 20), 4);
 
-              <div className="text-6xl text-white my-16 font-bold flex">
-                  <img width={60}  src={OpenCVLogo} className="mr-3"></img>
-                  OpenCV JS
-              </div>
-              <div className="flex justify-between justify-center">
-                  <div className="grid mx-4 border-gray border-8 rounded-xl p-4">
-                      <div className="text-white font-bold text-2xl text-center pb-2">
-                          Original Image
-                      </div>
-                      <img className="rounded-md" id="src" ref={img} src={WindahFace} onLoad={()=>setImgSrcLoaded(true)}/>
-                  </div>
+            cv.imshow("canvasOutput3", rgbImg);
+            imgProc.delete();
+        });
 
-                  <div className="grid mx-4 border-gray border-8 rounded-xl p-4">
-                      <div className="text-white font-bold text-2xl text-center pb-2">
-                          Grayscale
-                      </div>
-                      <canvas className="rounded-md" id="canvasOutput1" width="200" ></canvas>
-                  </div>
 
-                  <div className="grid mx-4 border-gray border-8 rounded-xl p-4">
-                      <div className="text-white font-bold text-2xl text-center pb-2">
-                          Image InRange
-                      </div>
-                      <canvas className="rounded-md" id="canvasOutput2" width="200" ></canvas>
-                  </div>
-                  <div className="grid mx-4 border-gray border-8 rounded-xl p-4">
-                      <div className="text-white font-bold text-2xl text-center pb-2">
-                          Canny Edge Detection
-                      </div>
-                      <canvas className="rounded-md" id="canvasOutput3" width="200" ></canvas>
-                  </div>
-              </div>
 
-          </div>
-        </>
-    );
-  }
+        useEffect(() => {
+            if (!openCVLoaded) {
+                const script = InjectScript("opencv", "https://docs.opencv.org/4.6.0/opencv.js");
+                script.then(() => {
+                    cv['onRuntimeInitialized'] = () => {
+                        setOpenCVLoaded(true);
+                    }
+                }).catch(() => {
+                    console.log("error");
+                })
+            }
+        }, [])
+
+//         useEffect(() => {
+//             if (openCVLoaded && imgSrcLoaded) {
+//                 let src = cv.imread(img.current!);
+//                 let dst = new cv.Mat();
+//                 cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+// // You can try more different parameters
+//                 cv.Canny(src, dst, 50, 100, 3, false);
+//                 cv.imshow('canvasOutput3', dst);
+//                 src.delete();
+//                 dst.delete();
+//             }
+//         }, [openCVLoaded, imgSrcLoaded])
+
+
+
+        return (
+            <>
+                <Navbar/>
+                <div className="bg-blue w-full bg-cover px-6 lg:px-0 min-h-screen flex flex-col items-center ">
+
+                    <div className="text-6xl text-white my-16 font-bold flex">
+                        <img width={60} src={OpenCVLogo} className="mr-3"></img>
+                        OpenCV JS
+                    </div>
+                    <div className="grid mb-8">
+                        <input type="file" onChange={(e) => {
+                            setFileUploaded(e.target.files![0]);
+                        }}/>
+                    </div>
+                    <div className="flex justify-between justify-center">
+                        <div className="grid w-1/2 mx-4 border-gray border-8 rounded-xl p-4">
+                            <div className="text-white font-bold text-2xl text-center pb-2">
+                                Original Image
+                            </div>
+                            <canvas className="rounded-md object-contain w-full" id="src" ref={img}/>
+                        </div>
+
+                        <div className="grid w-1/2 mx-4 border-gray border-8 rounded-xl p-4">
+                            <div className="text-white font-bold text-2xl text-center pb-2">
+                                Canny Edge Detection
+                            </div>
+                            <canvas className="rounded-md object-contain w-full" id="canvasOutput3" ref={imgOutput}/>
+                        </div>
+                    </div>
+
+                </div>
+            </>
+        );
+    }
 })
